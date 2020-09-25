d28b04c45598f96e2c30bd7c57596a39900f9e56
BANSO <bansoomoba@gmail.com>
2020-09-25 16:41:06 +0100

    changes made for bible doctrine repos

--- src/users/Signup.js
+++ src/users/Signup.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './loginStyle.css';
+import Bible from "./Login.jpg";
+ 
 
 
 function Signup() {
@@ -12,7 +14,7 @@ function Signup() {
       <div className="col-lg-6">
         <div className="card1 pb-5">
           <div className="row"> <img src="https://i.imgur.com/CXQmsmF.png" className="logo" /> </div>
-          <div className="row px-3 justify-content-center mt-4 mb-5 border-line"> <img src="https://i.imgur.com/uNGdWHi.png" className="image" /> </div>
+          <div className="row px-3 justify-content-center mt-4 mb-5 border-line"> <img src={Bible} className="image" /> </div>
         </div>
       </div>
       <div className="col-lg-6">
@@ -20,13 +22,7 @@ function Signup() {
           <div className="row mb-4 px-3">
             <h6 className="mb-0 mr-4 mt-2">Sign in with</h6>
             <div className="facebook text-center mr-3">
-              <div className="fa fa-facebook" />
-            </div>
-            <div className="twitter text-center mr-3">
-              <div className="fa fa-twitter" />
-            </div>
-            <div className="linkedin text-center mr-3">
-              <div className="fa fa-linkedin" />
+              <div className="fab fa-google-plus" />
             </div>
           </div>
           <div className="row px-3 mb-4">
@@ -52,14 +48,13 @@ function Signup() {
           <div className="row px-3 mb-4">
             <div className="custom-control custom-checkbox custom-control-inline"> <input id="chk1" type="checkbox" name="chk" className="custom-control-input" /> <label htmlFor="chk1" className="custom-control-label text-sm">Remember me</label> </div> <a href="#" className="ml-auto mb-0 text-sm">Forgot Password?</a>
           </div>
-          <div className="row mb-3 px-3"><Link to="/admin"> <button type="submit" className="btn btn-blue text-center">Signup</button> </Link></div>
+          <div className="row mb-3 px-3"><Link to="/admin"> <button type="submit" className="btn btn-white text-center">Signup</button> </Link></div>
           <div className="row mb-4 px-3"> <small className="font-weight-bold">Already have acount? <Link to="/" className="text-danger" >Login</Link></small> </div>
         </div>
       </div>
     </div>
-    <div className="bg-blue py-4">
+    <div className="bg-green py-4">
       <div className="row px-3"> <small className="ml-4 ml-sm-5 mb-2">Copyright © 2019. All rights reserved.</small>
-        <div className="social-contact ml-4 ml-sm-auto"> <span className="fa fa-facebook mr-4 text-sm" /> <span className="fa fa-google-plus mr-4 text-sm" /> <span className="fa fa-linkedin mr-4 text-sm" /> <span className="fa fa-twitter mr-4 mr-sm-5 text-sm" /> </div>
       </div>
     </div>
   </div>
